7ab1bf2756d857872294b318eaad67f40ae15016
Rafael Neto <rneto@users.noreply.github.com>
2021-09-09 07:04:19 +0200

    Update last-updated.vue

--- blog/.vuepress/components/last-updated.vue
+++ blog/.vuepress/components/last-updated.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="last-updated">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
+      <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" />
+    </svg>
     {{ text }} {{ $page.lastUpdated }}
   </div>
 </template>
@@ -8,7 +13,7 @@
   export default {
     data() {
       return {
-        text: 'Última modificación'
+        text: 'Última revisión'
       }
     }
   }
