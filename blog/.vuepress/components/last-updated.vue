e1f35b7df9b90e9f5e958b077d87cf9b1781eb2b
Rafael Neto <rneto@users.noreply.github.com>
2021-09-09 06:43:32 +0200

    Create last-updated.vue

---
+++ blog/.vuepress/components/last-updated.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="last-updated">
+    {{ text }} {{ $page.lastUpdated }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        text: 'Última modificación'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .last-updated {
+    display: inline-flex;
+    line-height: 12px;
+    font-size: 12px;
+    margin-right: 20px;
+  }
+</style>
